224182a528d9484b58d1bed0e789ce363e0d8763
Yashant Gyawali <yashantgyawali@gmail.com>
2022-08-29 18:57:13 +0545

    designed the home screen

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,9 +1,46 @@
 import styled from "styled-components";
+import Image from "next/image"
 
 export default function Home() {
-  return <Containr>HELLO</Containr>;
+  return <Container>
+    <LogoContainer>
+       {/* <img src={MainLogo} alt="Logo"/> */}
+       <Image src={"/asset/logo.svg"} height = {64} width = {256} />
+       </LogoContainer>
+      <Subheading>
+       two coding warriors face off real-time on an exciting programming challenge.
+       </Subheading>
+       <Form className="NicknameInput">
+  <Label>
+    Nickname:
+    <input className="inputBox" type="text" name="name" />
+  </Label>
+  <input class="primary-btn marginS" type="submit" value="Find a Warrior"/>
+</Form>
+
+  </Container>;
 }
 
-const Containr = styled.div`
-  color: red;
+const Container = styled.div`
+  margin-top: var(--spacingL);
+  display:flex;
+  flex-direction: column;
+  justify-content:center;
+  align-items:center;
+`;
+
+const LogoContainer = styled.div`
+
+`;
+const Form = styled.div`
+
+`;
+const Label = styled.div`
+
+`;
+
+const Subheading = styled.div`
+margin-top: var(--spacingS);
+font-weight: 300;
+font-size: 18px;
 `;
