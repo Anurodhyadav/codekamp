2a19f48e32b19abb6bdf347ad3bace5177257eb5
Sankalpa Pokhrel <saankalpaa@gmail.com>
2022-08-31 22:13:34 +0545

    fixed styled components error

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,50 @@
-import '../styles/globals.css'
-import '../styles/main.css'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import "../styles/globals.css";
+import "../styles/main.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const GlobalStyle = createGlobalStyle`
+ body {
+  padding: 0;
+  margin: 0;
+  font-family: 'Rubik', -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    font-family: 'Rubik', sans-serif;
+    color: var(--lightPurple);
+    background: var(--dark);
+    line-height: 150%;
+
+  @media screen and (max-width: 600px) {
+    padding: 0;
+    padding-left: var(--spacingXS);
+    padding-right: var(--spacingXS);  
+  }
 }
 
-export default MyApp
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+* {
+  box-sizing: border-box;
+}
+`;
+
+const theme = {
+  colors: {
+    primary: "#0070f3",
+  },
+};
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default MyApp;
