90959c80fea37bfb0c1ba89459df9fb0b3d5ad2f
Sankalpa Pokhrel <saankalpaa@gmail.com>
2022-09-07 07:48:39 +0545

    resolving issues with confetti

--- components/Winner.js
+++ components/Winner.js
@@ -3,31 +3,33 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 
 const Winner = () => {
-
-  const router = useRouter()
+  const router = useRouter();
 
   return (
-
     <ContainerFilter>
       <WinnerContainer>
         <Confetti className="bg-confetti-animated">
-        <WinnerImageContainer>
-          <Image src={"/asset/congratulations.svg"} height={200} width={200} />
-        </WinnerImageContainer>
-        <Points>+200 Points</Points>
-        <h1>CONGRATULATIONS</h1>
-        <p>
-          You emerged victorious. You may now battle more warriors and conquer
-          the the world of coding.
-        </p>
-        <ButtonContainer>
-          <button
-            className="primary-btn"
-            onClick={() => router.push("/matchpage")}
-          >
-            Find a new warrior
-          </button>
-        </ButtonContainer>
+          <WinnerImageContainer>
+            <Image
+              src={"/asset/congratulations.svg"}
+              height={200}
+              width={200}
+            />
+          </WinnerImageContainer>
+          <Points>+200 Points</Points>
+          <h1>CONGRATULATIONS</h1>
+          <p>
+            You emerged victorious. You may now battle more warriors and conquer
+            the the world of coding.
+          </p>
+          <ButtonContainer>
+            <button
+              className="primary-btn"
+              onClick={() => router.push("/matchpage")}
+            >
+              Find a new warrior
+            </button>
+          </ButtonContainer>
         </Confetti>
       </WinnerContainer>
     </ContainerFilter>
@@ -37,12 +39,12 @@ const Winner = () => {
 export default Winner;
 
 const ContainerFilter = styled.div`
-  position:absolute;
-  height:100vh;
-  width:100%;
+  position: absolute;
+  height: 100vh;
+  width: 100%;
   background: rgba(0, 0, 0, 0.9);
   z-index: 10;
-  `;
+`;
 
 const WinnerContainer = styled.div`
   display: flex;
@@ -57,9 +59,9 @@ const WinnerContainer = styled.div`
   background: #121212;
   border-radius: 8px;
   box-shadow: 0px 10px 20px rgb(0 0 0 / 50%);
-  width:75%;
-  padding:var(--spacingM); 
-  text-align:center;
+  width: 75%;
+  padding: var(--spacingM);
+  text-align: center;
 `;
 const Points = styled.p`
   color: #7ef535;
@@ -73,10 +75,10 @@ const ButtonContainer = styled.div`
 `;
 
 const Confetti = styled.div`
-  display:flex;
+  display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-}
 `;
 
+const WinnerImageContainer = styled.div``;
