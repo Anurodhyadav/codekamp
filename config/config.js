f85f7658d9291e99c42f8b4dc6f5fb30eb2e6ae6
Anurodhyadav <anurodhyadav072@gmail.com>
2023-01-26 15:32:46 +0545

    removing heroku url

--- config/config.js
+++ config/config.js
@@ -1,4 +1,2 @@
 const dev = process.env.NODE_ENV !== "production";
-export const server = dev
-  ? "http://localhost:3000"
-  : "https://codekamp.herokuapp.com";
+export const server = dev ? "http://localhost:3000" : "http://143.244.177.66/";
