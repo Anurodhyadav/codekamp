69726354aa14c17527f122c1005ddcecd31968da
Anurodhyadav <anurodhyadav072@gmail.com>
2023-01-26 14:59:12 +0545

    allowed control access

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,7 @@ export default function Home() {
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
     },
   });
 
