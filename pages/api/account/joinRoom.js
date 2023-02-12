269a46d59da3e4978c1462b6e3a7be4cbb8ac7d9
Anurodhyadav <anurodhyadav072@gmail.com>
2023-02-12 21:47:13 +0545

    commented out the join room code

--- pages/api/account/joinRoom.js
+++ pages/api/account/joinRoom.js
@@ -1,56 +1,56 @@
-import { getAvailbleRoomFromRedis, putInRedis } from "../../utils/redis";
+// import { getAvailbleRoomFromRedis, putInRedis } from "../../utils/redis";
 
-export default async function joinRoom(req, res) {
-  if (req.method == "POST") {
-    try {
-      const { user } = req.body;
+// export default async function joinRoom(req, res) {
+//   if (req.method == "POST") {
+//     try {
+//       const { user } = req.body;
 
-      const availableRooms = await getAvailbleRoomFromRedis();
+//       const availableRooms = await getAvailbleRoomFromRedis();
 
-      if (!availableRooms.length) {
-        const randomId = Date.now();
+//       if (!availableRooms.length) {
+//         const randomId = Date.now();
 
-        const roomInfo = {
-          id: randomId,
-          createdBy: user.name,
-          createdAt: Date.now(),
-          joinedBy: null,
-          started: false,
-        };
+//         const roomInfo = {
+//           id: randomId,
+//           createdBy: user.name,
+//           createdAt: Date.now(),
+//           joinedBy: null,
+//           started: false,
+//         };
 
-        await putInRedis(randomId, roomInfo);
+//         await putInRedis(randomId, roomInfo);
 
-        res
-          .status(200)
-          .send({ roomId: randomId, roomInfo: roomInfo, joining: false });
-        return;
-      }
-      let selectedRoomToJoin = null;
+//         res
+//           .status(200)
+//           .send({ roomId: randomId, roomInfo: roomInfo, joining: false });
+//         return;
+//       }
+//       let selectedRoomToJoin = null;
 
-      if (availableRooms.length === 1) {
-        selectedRoomToJoin = availableRooms[0];
-      } else {
-        const randomRoomIndex = Math.max(
-          0,
-          Math.ceil(Math.random() * availableRooms.length - 1)
-        );
-        selectedRoomToJoin = availableRooms[randomRoomIndex];
-      }
+//       if (availableRooms.length === 1) {
+//         selectedRoomToJoin = availableRooms[0];
+//       } else {
+//         const randomRoomIndex = Math.max(
+//           0,
+//           Math.ceil(Math.random() * availableRooms.length - 1)
+//         );
+//         selectedRoomToJoin = availableRooms[randomRoomIndex];
+//       }
 
-      const roomId = selectedRoomToJoin.id;
+//       const roomId = selectedRoomToJoin.id;
 
-      const reservation = {
-        ...selectedRoomToJoin,
-        joinedBy: user.name,
-        started: true,
-      };
+//       const reservation = {
+//         ...selectedRoomToJoin,
+//         joinedBy: user.name,
+//         started: true,
+//       };
 
-      await putInRedis(roomId, reservation);
+//       await putInRedis(roomId, reservation);
 
-      res.status(200).send({ roomId, roomInfo: reservation, joining: true });
-    } catch (error) {
-      console.error(error);
-      res.status(401).send({ error: "Something went wrong" });
-    }
-  }
-}
+//       res.status(200).send({ roomId, roomInfo: reservation, joining: true });
+//     } catch (error) {
+//       console.error(error);
+//       res.status(401).send({ error: "Something went wrong" });
+//     }
+//   }
+// }
--- pages/utils/redis.js
+++ pages/utils/redis.js
@@ -1,41 +1,41 @@
-import { createClient } from "redis";
+// import { createClient } from "redis";
 
-// const redisClient = createClient();
-const client = createClient();
+// // const redisClient = createClient();
+// const client = createClient();
 
-export const putInRedis = async (key, value) => {
-  // await client.connect();
-  return client.set(String(key), JSON.stringify(value));
-};
+// export const putInRedis = async (key, value) => {
+//   // await client.connect();
+//   return client.set(String(key), JSON.stringify(value));
+// };
 
-export const getFromRedis = async (key) => {
-  // const client = await redisClient.connect();
-  // await client.connect();
-  const value = await client.get(key);
-  return JSON.parse(value);
-};
+// export const getFromRedis = async (key) => {
+//   // const client = await redisClient.connect();
+//   // await client.connect();
+//   const value = await client.get(key);
+//   return JSON.parse(value);
+// };
 
-export const getAllFromRedis = async () => {
-  const valuesSet = [];
+// export const getAllFromRedis = async () => {
+//   const valuesSet = [];
 
-  console.log("The client", client);
+//   console.log("The client", client);
 
-  await client.connect();
+//   await client.connect();
 
-  console.log("The client again", client);
+//   console.log("The client again", client);
 
-  for await (const key of client.scanIterator()) {
-    // use the key!
-    const value = await client.get(key);
-    if (value) {
-      valuesSet.push(JSON.parse(value));
-    }
-  }
-  return valuesSet;
-};
+//   for await (const key of client.scanIterator()) {
+//     // use the key!
+//     const value = await client.get(key);
+//     if (value) {
+//       valuesSet.push(JSON.parse(value));
+//     }
+//   }
+//   return valuesSet;
+// };
 
-export const getAvailbleRoomFromRedis = async () => {
-  const allRooms = await getAllFromRedis();
+// export const getAvailbleRoomFromRedis = async () => {
+//   const allRooms = await getAllFromRedis();
 
-  return allRooms.filter((room) => !room.started && !room.joinedBy);
-};
+//   return allRooms.filter((room) => !room.started && !room.joinedBy);
+// };
